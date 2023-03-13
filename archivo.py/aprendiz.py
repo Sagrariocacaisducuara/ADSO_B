dba4d521c04fb0ce8363c57240575f8e5c3db071
Sagrariocacaisducuara <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-13 17:29:32 -0500

    Update aprendiz.py

--- archivo.py/aprendiz.py
+++ archivo.py/aprendiz.py
@@ -1,9 +1,8 @@
-class Aprendiz:
-    def __init__(self,fname,lname,email,id):
-        self.__fname=fname
-        self.__lname=lname
-        self.__email=email
-        self.__id=id
+class Aprendiz:#se crea una clases llamada aprendiz
+    def __init__(self,fname,lname,email,id):#se inicializa el contrustor con sus respectivos parametros
+        self.__fname=fname#se crea una variable de instancia con  su parametro
+        self.__lname=lname#se crea una variable de instancia con  su parametro
+        self.__email=email#se crea una variable de instancia con  su parametro
+        self.__id=id#se crea una variable de instancia con  su parametro
 
-    def nombreCompleto(self):
-        return self.__fname+' '+self.__lname
+    def nombreCompleto(self):#
