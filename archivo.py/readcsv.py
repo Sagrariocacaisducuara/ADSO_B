79a636b008cfe50a5cb9c1830442fc19b02c3785
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-30 08:02:11 -0500

    Update readcsv.py

--- archivo.py/readcsv.py
+++ archivo.py/readcsv.py
@@ -1,20 +1,22 @@
-from Aprendiz import *
-import csv
-listaAprendices=[]
+from Aprendiz import *# importar todas las funciones, clases y variables definidas en el archivo Aprendiz.py
+import csv#se importa el modulo csv
+listaAprendices=[]#se crea una lista vacia
 with open('C:\\Users\\usuario\\Documents\\01-SENA\\NetAcademy2023\\2693629.csv') as csvDataFile:
+#se utiliza para abrir un archivo CSV con la ruta de archivo especificada y crear un objeto de archivo CSV csvDataFile que se utiliza para leer los datos del archivo.
+
 #with open('files/people.csv') as csvDataFile:
 #with open('https://people.sc.fsu.edu/~jburkardt/data/csv/csv.html/addresses') as csvDataFile:
-    csvReader=csv.reader(csvDataFile)
+    csvReader=csv.reader(csvDataFile)#crea un objeto csv.reader que se puede utilizar para leer los datos del archivo CSV
     #print(csvReader)
-    for row in csvReader:
-        apr=Aprendiz(row[0],row[1],row[2],row[3])
-        listaAprendices.append(apr)
+    for row in csvReader:#crea un bucle for querecorre cada fila del archivo CSV que se ha abierto previamente y que se ha leído utilizando el objeto csv.reader
+        apr=Aprendiz(row[0],row[1],row[2],row[3])#crea un objeto de la clase "Aprendiz" utilizando los valores de la lista que representa una fila del archivo CSV.
+        listaAprendices.append(apr)#agrega el objeto apr que es una instancia de la clase Aprendiz a una lista llamada listaAprendices
         #print(row)
         #print('first name:',row[0])
         #print('last name:',row[1])
         #print('email:',row[2])
         #print('id:',row[3])
-print(listaAprendices)
-for apr in listaAprendices:
-    print(apr.nombreCompleto())
+print(listaAprendices)#imprime la listAprendices 
+for apr in listaAprendices: #recorre cada elemento en la lista listaAprendices y asigna temporalmente cada elemento a la variable apr
+    print(apr.nombreCompleto())#imprime el resultado de llamar al método nombreCompleto() del objeto apr
 
