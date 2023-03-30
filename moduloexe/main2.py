83cd3d6a680e2160df3f5968561907ff078e834f
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-30 10:02:49 -0500

    Update main2.py

--- moduloexe/main2.py
+++ moduloexe/main2.py
@@ -1,10 +1,10 @@
-from sys import path
+from sys import path#importa la variable path desde el módulo sys
 
-path.append('..\\MiPracticaPythonB\\modulos')
-print(path)
+path.append('..\\MiPracticaPythonB\\modulos')# agrega una nueva ruta de búsqueda al final de la lista 
+print(path)#imprime la lista de rutas de búsqueda que Python utilizará para encontrar módulos
 
-import extra.iota
-import extra.good.best.tau
+import extra.iota#importa el módulo iota desde el paquete extra
+import extra.good.best.tau# importa el módulo tau desde el subpaquete best del paquete good dentro del paquete extra
 
-print(extra.iota.FunI())
-print(extra.good.best.tau.FunT())
+print(extra.iota.FunI())#mprime el resultado devuelto por esa función
+print(extra.good.best.tau.FunT())#llama a la función FunT del módulo tau en el subpaquete best del paquete good dentro del paquete extra e imprime el resultado devuelto por esa función.
