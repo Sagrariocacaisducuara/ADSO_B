31874d1bcfb441ba748d84138297971a3b18ab5f
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-30 07:27:18 -0500

    Update aprendiz.py

--- archivo.py/aprendiz.py
+++ archivo.py/aprendiz.py
@@ -5,4 +5,5 @@ class Aprendiz:#se crea una clases llamada aprendiz
         self.__email=email#se crea una variable de instancia con  su parametro
         self.__id=id#se crea una variable de instancia con  su parametro
 
-    def nombreCompleto(self):#
+    def nombreCompleto(self): #crea una funcion con su constructor
+        return self.__fname+' '+self.__lname#se retorna la variable fname
