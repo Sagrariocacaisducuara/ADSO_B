d822c879a78b45ac134e763b3e49b0f1902d12f7
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-30 10:09:51 -0500

    Update main3.py

--- moduloexe/main3.py
+++ moduloexe/main3.py
@@ -1,13 +1,14 @@
-from sys import path
+from sys import path#importa la variable path desde el módulo sys de la biblioteca estándar de Python.
 
-path.append('..\\MiPracticaPythonB\\modulozip\\extrapack.zip')
+path.append('..\\MiPracticaPythonB\\modulozip\\extrapack.zip')# agrega una nueva ruta de búsqueda al final de la lista de rutas de búsqueda del módulo de Python
 
-import extra.good.best.sigma as sig
-import extra.good.alpha as alp
-from extra.iota import FunI
-from extra.good.beta import FunB
+import extra.good.best.sigma as sig #importa el módulo sigma del subpaquete best del paquete good dentro del paquete extra y lo asigna a un alias sig
+import extra.good.alpha as alp#importa el módulo alpha del paquete good dentro del paquete extra y lo asigna a un alias alp
+from extra.iota import FunI#mporta la función FunI directamente desde el módulo iota del paquete extra
 
-print(sig.FunS())
-print(alp.FunA())
-print(FunI())
-print(FunB())
+from extra.good.beta import FunB#importa la función FunB directamente desde el módulo beta del paquete good dentro del paquete extra
+
+print(sig.FunS())#muestra el resultado en la consola
+print(alp.FunA())#muestra el resultado en la consola.
+print(FunI())#muestra el resultado en la consola.
+print(FunB())#muestra el resultado en la consola.
