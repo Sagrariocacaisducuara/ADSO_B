25374a6a72276ef19a8396ab8ecc65facecf7255
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-31 08:53:37 -0500

    Update dbpyobjetos.py

--- sqlitepython/dbpyobjetos.py
+++ sqlitepython/dbpyobjetos.py
@@ -1,19 +1,19 @@
-from Persona import *
-import sqlite3
-lista=[]
-personas=[]
-with sqlite3.connect('sqlitepython/conpython.db')as con:
-    micursor=con.cursor()
-    sentencia="SELECT * FROM alumno;"
-    lista=micursor.execute(sentencia).fetchall()
+from Persona import * #se importa todo de la classe persona
+import sqlite3#se importa el modulo sqlite3
+lista=[]#crea una lista vacia 
+personas=[]#se crea otra lista vacia 
+with sqlite3.connect('sqlitepython/conpython.db')as con:##se conecta a la base de datos llamada conpython.db
+    micursor=con.cursor()#crea un objeto para ejecutar consultas SQL en una base de datos
+    sentencia="SELECT * FROM alumno;"#defini una sentencia SQL para consultar en la base de datos
+    lista=micursor.execute(sentencia).fetchall()ejecuta un consulta y almacena los datos en una lista de tuplas 
 #print(lista)
-for fila in lista:
-    id=fila[0]
-    nombre=fila[1]
-    apellido=fila[2]
-    email=fila[3]
-    ob=Persona(id,nombre,apellido,email)
-    personas.append(ob)
+for fila in lista:#recorre la lista que contiene las consultas SQL
+    id=fila[0]#asigna el valor del primer elemento de la lista 
+    nombre=fila[1]#asigna el valor del segundo elemento de la lista 
+    apellido=fila[2]#asigna el valor del tercer elemento de la lista 
+    email=fila[3]#asigna el valor del cuarto elemento de la lista 
+    ob=Persona(id,nombre,apellido,email)crea un objeto de la clase persona y lo asigna en la variable ob
+    personas.append(ob)#agrega un objeto a la lista persona 
 
-for objeto in personas:
-    print(objeto.getNombreCompleto())
+for objeto in personas:#recorre la lista persona 
+    print(objeto.getNombreCompleto())#imprime el resultado al llamar el metodo getNombreCompleto en el objeto de la clase
