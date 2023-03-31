1e6ba10f74085274b7ea85b68288f7e2d838f5b0
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-31 08:03:44 -0500

    Update dbpy1.py

--- sqlitepython/dbpy1.py
+++ sqlitepython/dbpy1.py
@@ -1,7 +1,7 @@
-import sqlite3
-with sqlite3.connect('sqlitepython/conpython.db')as con:
-    micursor=con.cursor()
-    sentencia="SELECT nombre,apellido FROM alumno;"
-    print(micursor.execute(sentencia).fetchmany(10))
+import sqlite3#importa el modulo sqlite3
+with sqlite3.connect('sqlitepython/conpython.db')as con: #se conecta a la base de datos llamada conpython.db
+    micursor=con.cursor()#crea un objeto para ejecutar consultas SQL en una base de datos
+    sentencia="SELECT nombre,apellido FROM alumno;"# definir una sentencia SQL 
+    print(micursor.execute(sentencia).fetchmany(10))imprime la consulta SQL de la base de datos
 
 #print()
