f96604c1e1d0c86e514bc6b02809a1e561348e80
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-31 10:48:51 -0500

    Update Vehiculo.py

--- poo-archivos/Vehiculo.py
+++ poo-archivos/Vehiculo.py
@@ -1,8 +1,8 @@
-class Vehiculo:
-    def __init__(self,placa,conductor):
-        self.__placa=placa
-        self.__conductor=conductor
-    def getConductor(self):
-        return self.__conductor
-    def getPlaca(self):
-        return self.__placa
+class Vehiculo:#se crea la case Vehiculo
+    def __init__(self,placa,conductor):#se inicialoiza su constructor con sus respectivos atributos
+        self.__placa=placa# se asigna el valor a el atribito placa
+        self.__conductor=conductor# se asigna el valor a el atribito conductor
+    def getConductor(self):#define un metodo en la clase
+        return self.__conductor#se retorna el valor del atriburo conductor
+    def getPlaca(self)::#define un metodo en la clase
+        return self.__placa#se retorna el valor del atriburo placa
