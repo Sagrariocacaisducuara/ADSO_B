0d0d70c4a9697a76625bc6844287d0d4a7c578fe
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-31 09:30:12 -0500

    Update Carga.py

--- poo-archivos/Carga.py
+++ poo-archivos/Carga.py
@@ -1,10 +1,10 @@
-from Vehiculo import *
-class Carga(Vehiculo):
-    def __init__(self,placa,conductor,capacidad,ejes):
-        Vehiculo.__init__(self,placa,conductor)
-        self.__capacidad=capacidad
-        self.__ejes=ejes    
-    def getCapacidad(self):
-        return self.__capacidad    
-    def getEjes(self):
-        return self.__ejes
+from Vehiculo import *#se importa el modulo vehiculo 
+class Carga(Vehiculo):#se crea una clase llamada carga que viene de otra clase vehiculo
+    def __init__(self,placa,conductor,capacidad,ejes):#se ninializa su constrcutor con 5 parametros
+        Vehiculo.__init__(self,placa,conductor)#se instancia la clase vehiculo
+        self.__capacidad=capacidad#se crea una variable de instancia
+        self.__ejes=ejes    #se crea una variable de instancia
+    def getCapacidad(self):#se crea el metodo getCapacidad
+        return self.__capacidad #se retorna la variable capacidad
+    def getEjes(self):#se crea el metodo getEjes
+        return self.__ejes#se retorna la variable ejes
