c9ff146f3ee038f4d1a3381ba74263896f0583c8
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-31 09:18:10 -0500

    Update Persona.py

--- poo/Persona.py
+++ poo/Persona.py
@@ -1,35 +1,36 @@
-class Persona:    
-    nacionalidad='Colombia'
-    def __init__(self,nombre,documento):
-        self.__nombre=nombre
-        self.__documento=documento
-        print('Activando constructor')
+class Persona:#cre crea la clase persona 
+    nacionalidad='Colombia'#asigna un  un valor a la variable nacinalidad
+    def __init__(self,nombre,documento):#se inicialoiza su constructor con sus respectivos atributos
+        self.__nombre=nombre# se asigna el valor a el atributo privado nombre
+        self.__documento=documento# se asigna el valor a el atributo documento
+        print('Activando constructor')#imprime activando constructor
 
-    def getNombre(self):
-        return self.__nombre
+    def getNombre(self):#define un metodo en la clase  
+        return self.__nombre#devuelve el valor del atriburo nombre
     
-    def setNombre(self, nombre):
-        self.__nombre=nombre
+    def setNombre(self, nombre):#define un metodo en la clase para el valor del atributo nombre
+        self.__nombre=nombre#asigna el valor al tributo nombre 
 
-    def metodo(self,mail):
-        self.__mail=mail
-        #print('Soy un método')
+    def metodo(self,mail):#define un metodo en la clase para el valor del atributo mail
+        self.__mail=mail#asigna el valor al tributo nombre 
+        #print('Soy un método')#imprime soy un metodo
 
 
-ob=Persona('Ana',12345)
-ob.mail='ana@mail.com'
-print(ob.mail)
-ob1=Persona('Luis',54321)
-print(ob.nacionalidad)
-print(ob1.nacionalidad)
-print(Persona.nacionalidad)
 
-print(ob.getNombre())
-ob.setNombre('Maria')
-print(ob.getNombre())
+ob=Persona('Ana',12345)#crea un objeto de la clase Persona con los atributos nombre y id 
+ob.mail='ana@mail.com'#asignar un valor al atributo mail del objeto ob de la clase Persona
+print(ob.mail)#imprime el objeto mail
+ob1=Persona('Luis',54321)#crea un objeto de la clase Persona con los atributos nombre y id 
+print(ob.nacionalidad)imprimi el valor del atributo nacionalidad del objeto ob de la clase Persona
+print(ob1.nacionalidad)imprimi el valor del atributo nacionalidad del objeto ob de la clase Persona
+print(Persona.nacionalidad)imprimi el valor del atributo nacionalidad de la clase "Persona"
+
+print(ob.getNombre())#mprimir el valor retornado por el método getNombre() del objeto ob de la clase Persona
+ob.setNombre('Maria')#llamar al método setNombre() del objeto ob de la clase Persona, y así establecer un nuevo valor para el atributo nombre
+print(ob.getNombre())#imprimir el valor actualizado del atributo nombre del objeto ob de la clase Persona
 #ob.metodo()
 #print(type(ob))
 
-cadena="SENA"
-print(cadena.lower())
-print(type(cadena))
+cadena="SENA"#asignar la cadena de caracteres SENA a la variable cadena
+print(cadena.lower())#imprimir la variable cadena en minúsculas
+print(type(cadena))#para imprimir el tipo de dato de la variable cadena.
