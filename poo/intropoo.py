98b95cbfc0af447db1d598bd9bbd7581c566693e
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-31 09:21:21 -0500

    Update intropoo.py

--- poo/intropoo.py
+++ poo/intropoo.py
@@ -1,7 +1,7 @@
 #NO tipado
 x=10
 x=[]
-cad='Amo la programación'
-print(type(cad))
-print(len(cad))
-print(cad.capitalize())
+cad='Amo la programación' #asignar la cadena de caracteres a la variable 
+print(type(cad))#se utiliza para imprimir el tipo de dato de la variable cad
+print(len(cad)) imprimir la longitud de la variable cad
+print(cad.capitalize())mprimir la variable cad con la primera letra en mayúscula y las demás en minúscula
