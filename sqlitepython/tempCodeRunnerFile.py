f35e6d7bd6562df305bd479776e1824f44db2637
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-31 08:56:42 -0500

    Update tempCodeRunnerFile.py

--- sqlitepython/tempCodeRunnerFile.py
+++ sqlitepython/tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
-import sqlite3
-con=sqlite3.connect('C:\\narvaez\\db\\conpython.db')
-print(type(con))
-micursor=con.cursor()
-print(type(micursor))
+import sqlite3#importa el modulo sqlite3
+con=sqlite3.connect('C:\\narvaez\\db\\conpython.db')#se conecta a la base de datos llamada conpython.db
+print(type(con))#imprime el tipo de dato 
+micursor=con.cursor()#crea un objeto para ejecutar consultas SQL en una base de datos
+print(type(micursor))#imprime el tipo de dato del cursor
