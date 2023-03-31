b486876fecc1f759cb7ed777e3c8a405254734c0
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-31 10:46:07 -0500

    Update Conductor.py

--- poo-archivos/Conductor.py
+++ poo-archivos/Conductor.py
@@ -1,8 +1,8 @@
-class Conductor:
-    def __init__(self,nombre,documento):
-        self.__nombre=nombre
-        self.__documento=documento
-    def getNombre(self):
-        return self.__nombre
-    def getDocumento(self):
-        return self.__documento
+class Conductor:#se crea la clase persona
+    def __init__(self,nombre,documento):#se inicialoiza su constructor con sus respectivos atributos
+        self.__nombre=nombre# se asigna el valor a el atributo mnonbre
+        self.__documento=documentose asigna el valor a el atributo documento
+    def getNombre(self):#define un metodo en la clase #devuelve el valor del atriburo nombre
+        return self.__nombre#retorna el valor del atriburo nombre
+    def getDocumento(self):#define un metodo en la clase para el valor del atributo documento
+        return self.__documento#retorna el valor del atriburo documento
