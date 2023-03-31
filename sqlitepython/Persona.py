24b9aa8ded2d2de8c23e686b04705feea92d64e9
sagrario <116923031+Sagrariocacaisducuara@users.noreply.github.com>
2023-03-31 07:56:52 -0500

    Update Persona.py

--- sqlitepython/Persona.py
+++ sqlitepython/Persona.py
@@ -1,16 +1,16 @@
-class Persona:    
+class Persona:#se crea la clase persona
     
-    def __init__(self,id,nombre,apellido,email):
-        self.__id=id
-        self.__nombre=nombre
-        self.__apellido=apellido
-        self.__email=email
+    def __init__(self,id,nombre,apellido,email):#se inicialoiza su constructor con sus respectivos atributos
+        self.__id=id # se asigna el valor a el atributo privado id 
+        self.__nombre=nombre# se asigna el valor a el atributo nombre
+        self.__apellido=apellido# se asigna el valor a el atributo apellido
+        self.__email=email# se asigna el valor a el atributo email
 
-    def getNombre(self):
-        return self.__nombre
+    def getNombre(self): #define un metodo en la clase  s
+        return self.__nombre#devuelve el valor del atriburo nombre
     
-    def setNombre(self, nombre):
-        self.__nombre=nombre
+    def setNombre(self, nombre):#define un metodo en la clase para el valor del atributo nombre
+        self.__nombre=nombre#asigna el valor al tributo nombre 
 
-    def getNombreCompleto(self):
-        return self.__nombre+' '+self.__apellido
+    def getNombreCompleto(self):#define un metodo en la clase
+        return self.__nombre+' '+self.__apellido#retorna el atributo completo de un objeto de la clase persona 
